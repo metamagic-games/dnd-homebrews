5fa06aaf81db2857fdd957de498af82fcc947eea
Max Clayton Clowes <contact@mcclowes.com>
2021-03-03 17:54:04 +0000

    [feat] tidy up createrulebooks

--- scripts/createRulebooks.js
+++ scripts/createRulebooks.js
@@ -5,25 +5,7 @@ const options = {
   printOptions: {
     displayHeaderFooter: false,
   },
-  markdownOptions: {
-    //"baseUrl": null,
-    // "breaks": false,
-    // "gfm": true,
-    // "headerIds": true,
-    // "headerPrefix": "",
-    // "highlight": null,
-    // "langPrefix": "language-",
-    // "mangle": true,
-    // "pedantic": false,
-    // "sanitize": false,
-    // "sanitizer": null,
-    // "silent": false,
-    // "smartLists": false,
-    // "smartypants": false,
-    // "tokenizer": null,
-    // "walkTokens": null,
-    // "xhtml": false
-  },
+  markdownOptions: {},
 };
 
 const paths = {
@@ -43,32 +25,28 @@ const paths = {
   warden: "./src/Classes/Warden/Warden",
 };
 
-const createRulebooks = async () => {
+export const createRulebooks = async (rules, options=options) => {
   console.log("Creating rulebooks...");
 
-  const cvs = process.argv.slice(2);
+  for (let i = 0; i < rules.length; i++) {
+    const rule = paths[rules[i]];
 
-  if (cvs.length > 0) {
-    for (let i = 0; i < cvs.length; i++) {
-      const rule = paths[cvs[i]];
+    console.log("\n>>>", rule);
 
-      console.log("\n>>>", rule);
-
-      await handbooker(`${rule}.md`, `${rule}.pdf`, options);
-    }
-  } else {
-    const documents = Object.keys(paths);
-
-    for (let i = 0; i < documents.length; i++) {
-      const rule = documents[i];
-
-      console.log("\n>>>", rule);
-
-      await handbooker(`${rule}.md`, `${rule}.pdf`, options);
-    }
+    await handbooker(`${rule}.md`, `${rule}.pdf`, options);
   }
 
   console.log("\nFinished!");
 };
 
-createRulebooks();
+const createRulebooksFromCLI = () => {
+  const commands = process.argv.slice(2);
+
+  if (commands.length > 0) {
+    createRulebooks(commands) 
+  } else {
+    createRulebooks(Object.keys(paths));
+  }
+}
+
+createRulebooksFromCLI();
